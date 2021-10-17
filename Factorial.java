6fe006f0ae5ad8cfdc9bf461edcb5025530cd070
chandoleavadhut <91368001+chandoleavadhut@users.noreply.github.com>
2021-10-17 19:02:35 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Factorial{
+
+    public static void main(String[] args) {
+        
+    Scanner sc=new Scanner(System.in);
+    System.out.println(Enter the number );
+    int num =sc.nextInt();
+    long factorial = multiplyNumbers(num);
+    System.out.println(Factorial of  + num +  =  + factorial);
+    }
+    public static long multiplyNumbers(int num)
+    {
+        if (num = 1)
+            return num  multiplyNumbers(num - 1);
+        else
+            return 1;
+    }
+}
